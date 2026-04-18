7fb72b17ed793e1ee8cf78ae84141d43b0a6520e
taipeiedyen-bit <taipeiedyen@gmail.com>
2026-04-18 13:42:54 +0800

    Create _document.tsx for Next.js HTML structure with meta tags

---
+++ _document.tsx
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="description" content="Ocean immersion experience" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=YourCustomFont:wght@400;700&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
